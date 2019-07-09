c9b6d92c44f94b7bdeaa48e420aad6d427535e95
Sheng-Fu Wang <shengfu.wang@nyu.edu>
2019-07-09 09:17:30 -0400

    Update dictionary_corpus.py

--- src/language_models/dictionary_corpus.py
+++ src/language_models/dictionary_corpus.py
@@ -114,7 +114,10 @@ def sent_tokenize_with_unks(dictionary, path):
         with open(path, 'r+', encoding="utf-8") as f:
             for line in f:
                 sents.append(line.strip())
-                words = ['<eos>'] + line.split()
+                words = ['<eos>'] + line.split
+                print(words)
+                exit()
+
                 tokens = len(words)
                 
                 # tokenize file content
