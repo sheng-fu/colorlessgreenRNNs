acdff004917ae355e66f477e7c85137f3a7dc551
Sheng-Fu Wang <shengfu.wang@nyu.edu>
2019-07-15 11:31:52 -0400

    added dummy segment

--- src/language_models/json_to_txt.py
+++ src/language_models/json_to_txt.py
@@ -9,12 +9,12 @@ infile_json = [json.loads(x) for x in infile]
 
 sentences = []
 
-outfile = open('../../data/sent_pair/test_simp.txt', 'w')
+outfile = open('../../data/sent_pair/test_simp_altered.txt', 'w')
 
 for i in infile_json:
-    outfile.write(re.sub('(\\.)', ' \\1 <eos>', i['sentence_good']))
+    outfile.write(re.sub('(\\.)', ' and this is false \\1 <eos>', i['sentence_good']))
     outfile.write('\n')
-    outfile.write(re.sub('(\\.)', ' \\1 <eos>', i['sentence_bad']))
+    outfile.write(re.sub('(\\.)', ' and this is false \\1 <eos>', i['sentence_bad']))
     outfile.write('\n')
 
 
