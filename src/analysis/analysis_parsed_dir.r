753db91224c50485ca39c45a702309e2b539279a
Sheng-Fu Wang <shengfu.wang@nyu.edu>
2019-08-09 07:26:17 +1000

    gender agreement fix

--- src/analysis/analysis_parsed_dir.r
+++ src/analysis/analysis_parsed_dir.r
@@ -47,9 +47,32 @@ for (file in prefix_file){
   
 }
 
+two_prefix_path = "~/GitHub/colorlessgreenRNNs/results/two_prefix_full/"
+two_prefix_file = list.files(two_prefix_path)
+two_prefix_file = two_prefix_file[grepl("_parsed", two_prefix_file)]
+two_prefix_file
+
+result_two_prefix = tibble()
+for (file in two_prefix_file){
+  result = read_tsv(paste(two_prefix_path, file, sep = ""), col_names = c("good_prob", "bad_prob"))
+  no_ext = gsub("_output_parsed\\.tab", "", file)
+  data = stream_in(file(paste(jsonl_path, no_ext, ".jsonl", sep="")))
+  data = as_data_frame(data)
+  data = select(data, sentence_good, sentence_bad,  
+                field, linguistics_term, UID, 
+                simple_LM_method, one_prefix_method, two_prefix_method, 
+                lexically_identical)
+  result = cbind(data, result)
+  #print(colnames(result))
+  result_two_prefix = rbind(result_two_prefix, result)
+  
+}
+
+
 result_sent$type = "sentence"
-result_prefix$type = "prefix"
-result_all = rbind(result_sent, result_prefix)
+result_prefix$type = "one-prefix"
+result_two_prefix$type = "two-prefix"
+result_all = rbind(result_sent, result_prefix, result_two_prefix)
 
 result_all$prob_dec = result_all$good_prob < result_all$bad_prob
 
